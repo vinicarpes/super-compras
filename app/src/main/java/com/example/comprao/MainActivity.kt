7ebc169d6bb8496ed476f3cfee5fbc4986b08f5d
vinicarpes <vinicdf000@gmail.com>
2025-08-12 15:58:41 -0300

    feat: add button to add items

--- app/src/main/java/com/example/comprao/MainActivity.kt
+++ app/src/main/java/com/example/comprao/MainActivity.kt
@@ -9,20 +9,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -39,14 +48,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             CompraoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(verticalArrangement = Arrangement.Top) {
-                        ImagemTopo(modifier = Modifier.padding(innerPadding))
+                    Column(
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        ImagemTopo()
+                        AdicionarItem()
+                        BotaoSalvarItem(onClick = {})
                         Titulo(
-                            texto = "Lista de Compras",
-                            modifier = Modifier.padding(innerPadding)
+                            texto = "Lista de Compras"
                         )
-                        ItemDaLista(modifier = Modifier.padding(innerPadding))
-                        Titulo(texto = "Comprado", modifier = Modifier.padding(innerPadding))
+                        ItemDaLista()
+                        Titulo(texto = "Comprado")
                     }
 
                 }
@@ -92,11 +106,34 @@ fun ItemDaLista(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun AdicionarItem(modifier: Modifier = Modifier) {
+    var texto = rememberSaveable { mutableStateOf("") }
+    OutlinedTextField(
+        placeholder = {
+            Text(
+                text = "Digite o item que deseja adicionar",
+                color = Color.Gray,
+                style = Typography.bodyMedium
+            )
+        },
+        value = texto.value,
+        onValueChange = { texto.value = it },
         modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        singleLine = true,
+        shape = RoundedCornerShape(24.dp)
     )
+}
+
+@Composable
+fun BotaoSalvarItem(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(shape = RoundedCornerShape(24.dp), onClick = onClick, modifier = modifier) {
+        Text(
+            text = "Salvar Item",
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp))
+    }
 }
 
 @Composable
@@ -111,10 +148,7 @@ fun ImagemTopo(modifier: Modifier = Modifier) {
 @Composable
 fun Icone(icone: ImageVector, modifier: Modifier = Modifier) {
     Icon(
-        icone,
-        contentDescription = "Editar",
-        modifier,
-        tint = Marinho
+        icone, contentDescription = "Editar", modifier, tint = Marinho
     )
 }
 
@@ -129,6 +163,12 @@ private fun ItemDaListaPreview() {
     CompraoTheme {
         ItemDaLista()
     }
+}
+
+@Preview
+@Composable
+private fun AdicionarItemPreview() {
+    CompraoTheme { AdicionarItem() }
 }
 
 @Preview
@@ -155,10 +195,12 @@ fun TituloPreview() {
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun SalvarItemPreview() {
     CompraoTheme {
-        Greeting("Jorge")
+        BotaoSalvarItem(onClick = {})
     }
+
 }
